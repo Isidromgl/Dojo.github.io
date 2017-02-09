aa6659150d36ad3a444629d163b5f74d70884992
Isidromgl <isidromgl@gmail.com>
2017-02-09 20:00:42 +0100

    Incluido un efecto Dojo

---
+++ js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+var element = document.getElementById('Estudios');
+element.style="color:white" ;
+console.log("element",element.getAttribute("style"));
+
+var $h2 = $("h2");
+$h2.click(function(){
+
+})
